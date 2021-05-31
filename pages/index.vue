f13de333b05ac6e63267e34b0e22b44da36901e8
nasiruddin01 <nasiruddin.dev@gmail.com>
2021-05-31 23:17:37 +0600

    home page css implement

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
-    <div v-if="pokemon">
-      <p>Total{{ pokemon.count }}</p>
-      <div v-for="result in pokemon.results" :key="result.id">
-        <p>{{ result.name }}</p>
-        <img
-          :src="'https://lorempokemon.fakerapi.it/pokemon/200/' + result.name"
-          alt=""
-        />
+    <h1>Pokemon</h1>
+    <div class="search-bar">
+      <input type="text" placeholder="search your pokemon" />
+      <button class="btn">Submit</button>
+    </div>
+    <div v-if="pokemon" class="pokemon">
+      <div v-for="result in pokemon.results" :key="result.id" class="card">
+        <a :href="'http://localhost:3000' + pokemon.id" target="_blank"
+          ><img
+            :src="'https://lorempokemon.fakerapi.it/pokemon/200/' + result.name"
+            alt=""
+          />
+          <p>{{ result.name }}</p></a
+        >
       </div>
-
+      <p>Total{{ pokemon.count }}</p>
       <a :href="pokemon.next" target="_blank">next</a>
     </div>
   </div>
@@ -57,14 +63,74 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  max-width: 1080px;
-  width: 95%;
-  // background-color: #edecf0;
-  margin: 0 auto;
+  background-color: #edecf0;
+  width: 100vw;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  h1 {
+    text-align: center;
+    padding-top: 1rem;
+    margin-bottom: 1rem;
+  }
+  .search-bar {
+    width: 30%;
+    margin: 0 auto;
+
+    input {
+      padding: 0.5rem 3rem;
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+    }
+    input:focus {
+      outline: none;
+    }
+    .btn {
+      padding: 0.5rem 2rem;
+      transform: translateX(-5px);
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+    }
+  }
+  .pokemon {
+    max-width: 1080px;
+    width: 95%;
+    margin: 2rem auto 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 1rem;
+    @media (max-width: 768px) {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-gap: 1rem;
+    }
+    .card {
+      width: 100%;
+      margin: 0 auto;
+      background-color: #ffff;
+      border: 1px solid #d2d7ee;
+      border-radius: 5px;
+      @media (max-width: 768px) {
+        width: 90%;
+        margin: 0 auto;
+      }
+      a {
+        text-align: center;
+        text-decoration: none;
+        text-transform: capitalize;
+        color: #000;
+        img {
+          width: 90%;
+          margin: 0.75rem auto 0 auto;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+        }
+        p {
+          padding-bottom: 0.75rem;
+          font-weight: 400;
+          touch-action: pan-x;
+        }
+      }
+    }
+  }
 }
 </style>
